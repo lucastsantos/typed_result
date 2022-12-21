4cb2b5253109fb0dc7d0dc040496a615ffc4c6ee
Lucas Teixeira <luc.tsantos@gmail.com>
2022-12-20 21:52:17 -0300

    Improving (or trying to) example/main.dart readability

--- example/main.dart
+++ example/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:typed_result/typed_result.dart';
 
 class DateException implements Exception {}
@@ -17,105 +19,124 @@ Result<DateTime, DateException> getDate(bool forceFailure) {
   }
 }
 
-void main() {
-  "Creating instances of Result".log();
-  var ok = getDate(false);
-  ok.log();
-  "ok.isSuccess: ${ok.isSuccess}".log();
-  "ok.isFailure: ${ok.isFailure}".log();
-  var err = getDate(true);
-  err.log();
-  "err.isSuccess: ${err.isSuccess}".log();
-  "err.isFailure: ${err.isFailure}".log();
+var ok = getDate(false);
+var err = getDate(true);
 
-  "\n`get`".log();
-  getDate(false).get().log();
-  getDate(true).get().log();
+/// Sample for showing what attributes a Result instance has.
+void instances() {
+  print("\nInspecting instances of Result");
+  print(ok);
+  print("ok.isSuccess: ${ok.isSuccess}");
+  print("ok.isFailure: ${ok.isFailure}");
 
-  "\n`getOr`".log();
-  getDate(false).getOr(() => DateTime(2000)).log();
-  getDate(true).getOr(() => DateTime(2000)).log();
-
-  "\n`getOrThrow`".log();
-  getDate(false).getOrThrow().log();
-  try {
-    getDate(true).getOrThrow().log();
-  } catch (e) {
-    "Exception was thrown because it was a failure result".log();
-  }
-
-  "\n`getError`".log();
-  getDate(false).getError().log();
-  getDate(true).getError().log();
-
-  "\n`getErrorOr`".log();
-  getDate(false).getErrorOr(() => ChildDateException()).log();
-  getDate(true).getErrorOr(() => ChildDateException()).log();
-
-  "\n`getErrorOrThrow`".log();
-  try {
-    getDate(false).getErrorOrThrow().log();
-  } catch (e) {
-    "Exception was thrown because it was a success result".log();
-  }
-  getDate(true).getErrorOrThrow().log();
-
-  "\n`map`".log();
-  getDate(false).map((_) => "Mapping success").log();
-  getDate(true)
-      .map((_) => "Nothing happens; still an instance of DateException")
-      .log();
-
-  "\n`mapError`".log();
-  getDate(false)
-      .mapError((_) => "Nothing happens; still an instance of DateTime")
-      .log();
-  getDate(true).mapError((_) => "Mapping error").log();
-
-  "\n`mapBoth`".log();
-  getDate(false)
-      .mapBoth(
-        success: (_) => "Mapping success with failure alternative",
-        failure: (_) => "Nothing happens",
-      )
-      .log();
-  getDate(true)
-      .mapBoth(
-        success: (_) => "Nothing happens",
-        failure: (_) => "Mapping failure with success alternative",
-      )
-      .log();
-
-  "\n`on`".log();
-  getDate(false)
-      .onSuccess((_) => "On Success".log())
-      .onFailure((_) => "Nothing happens".log());
-  getDate(true)
-      .onSuccess((_) => "Nothing happens".log())
-      .onFailure((_) => "On Failure".log());
-
-  "\n`when`".log();
-  getDate(false).when(
-    success: (_) => "When [success]".log(),
-    failure: (_) => "Nothing happens".log(),
-  );
-  getDate(true).when(
-    success: (_) => "Nothing happens".log(),
-    failure: (_) => "When [failure]".log(),
-  );
-
-  "\n`runCatching`".log();
-  runCatching(() => DateTime.now()).log();
-  runCatching(() => throw DateException()).log();
-
-  "\n`toResult`".log();
-  DateTime.now().toResultOr(() => DateException()).log();
-  null.toResultOr(() => DateException()).log();
+  print(err);
+  print("err.isSuccess: ${err.isSuccess}");
+  print("err.isFailure: ${err.isFailure}");
 }
 
-/// Extension just to avoid the relevant code to be presented inside a parenthesis
-extension Print on Object? {
-  void log() {
-    print(this);
+/// Sample for showing how to get the values from a Result.
+void getters() {
+  print("\n`get`");
+  print(ok.get());
+  print(err.get());
+
+  print("\n`getOr`");
+  print(ok.getOr(() => DateTime(2000)));
+  print(err.getOr(() => DateTime(2000)));
+
+  print("\n`getOrThrow`");
+  print(ok.getOrThrow());
+  try {
+    err.getOrThrow();
+  } catch (e) {
+    print("Exception was thrown because it was a failure result");
   }
+
+  print("\n`getError`");
+  print(ok.getError());
+  print(err.getError());
+
+  print("\n`getErrorOr`");
+  print(ok.getErrorOr(() => ChildDateException()));
+  print(err.getErrorOr(() => ChildDateException()));
+
+  print("\n`getErrorOrThrow`");
+  try {
+    ok.getErrorOrThrow();
+  } catch (e) {
+    print("Exception was thrown because it was a success result");
+  }
+  print(err.getErrorOrThrow());
+}
+
+/// Sample for showing how to transforming the current Result value into a new Result.
+void map() {
+  print("\n`map`");
+  print(ok.map((value) => "Mapping success at $value"));
+  print(err
+      .map((value) => "Nothing happens; still an instance of DateException"));
+
+  print("\n`mapError`");
+  print(ok.mapError((_) => "Nothing happens; still an instance of DateTime"));
+  print(err.mapError((_) => "Mapping error"));
+
+  print("\n`mapBoth`");
+  print(ok.mapBoth(
+    success: (_) =>
+        "Mapping success with a failure alternative that was not called",
+    failure: (_) => "Nothing happens",
+  ));
+  print(err.mapBoth(
+    success: (_) => "Nothing happens",
+    failure: (_) =>
+        "Mapping failure with a success alternative that was not called",
+  ));
+}
+
+/// Sample for showing how to invoke block based on the Result outcome.
+void on() {
+  print("\n`on`");
+  ok
+      .onSuccess((_) => print("onSuccess"))
+      .onFailure((_) => print("Nothing happens"));
+  err
+      .onSuccess((_) => print("Nothing happens"))
+      .onFailure((_) => print("onFailure"));
+}
+
+/// Sample for showing an alternative on how to invoke block based on the Result outcome.
+void when() {
+  print("\n`when`");
+  ok.when(
+    success: (_) => print("When [success]"),
+    failure: (_) => print("Nothing happens"),
+  );
+  err.when(
+    success: (_) => print("Nothing happens"),
+    failure: (_) => print("When [failure]"),
+  );
+}
+
+/// Sample for showing how safely call a method, transforming any exception into a Result error.
+void catching() {
+  print("\n`runCatching`");
+  print(runCatching(() => DateTime.now()));
+  print(runCatching(() => throw DateException()));
+}
+
+/// Sample for showing how to transform any object into a Result, based on it's nullability.
+void toResult() {
+  print("\n`toResult`");
+  print(DateTime.now().toResultOr(() => DateException()));
+  print(null.toResultOr(() => DateException()));
+}
+
+void main() {
+  instances();
+  getters();
+  map();
+  on();
+  when();
+  catching();
+  toResult();
 }
