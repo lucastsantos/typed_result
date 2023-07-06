8323c4a21c951fc8e2bbdf459ea6e08c93a13504
Lucas Teixeira <luc.tsantos@gmail.com>
2023-07-05 22:18:19 -0300

    Adding extensions for Future

--- lib/src/functions/on.dart
+++ lib/src/functions/on.dart
@@ -21,3 +21,15 @@ extension OnResult<T, E> on Result<T, E> {
     return this;
   }
 }
+
+extension FutureOnResult<T, E> on Future<Result<T, E>> {
+  /// Invokes [block] only if this is a success.
+  Future<Result<T, E>> onSuccess(void Function(T value) block) {
+    return then((result) => result.onSuccess(block));
+  }
+
+  /// Invokes [block] only if this is a failure.
+  Future<Result<T, E>> onFailure(void Function(E error) block) {
+    return then((result) => result.onFailure(block));
+  }
+}
