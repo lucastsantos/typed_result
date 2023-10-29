fd4c0046753a7bdc2c801c25e6ed6c6307ec8614
Lucas Teixeira <luc.tsantos@gmail.com>
2023-10-29 17:06:30 -0300

    Adding getAllErrors method (#10)

--- test/functions/get_all_test.dart
+++ test/functions/get_all_test.dart
@@ -8,7 +8,7 @@ void main() {
         const Err(1),
         const Ok(2),
         const Ok(3),
-        const Err(4)
+        const Err(4),
       ];
       expect(results.getAll(), [2, 3]);
     });
@@ -21,6 +21,28 @@ void main() {
     test('should return empty list if has only failures', () {
       List<Result<int, int>> results = [const Err(1), const Err(2)];
       expect(results.getAll(), isEmpty);
+    });
+  });
+
+  group('getAllErrors', () {
+    test('should return error values from iterables', () {
+      List<Result<int, int>> results = [
+        const Ok(1),
+        const Err(2),
+        const Err(3),
+        const Ok(4),
+      ];
+      expect(results.getAllErrors(), [2, 3]);
+    });
+
+    test('should return empty list if has no elements', () {
+      List<Result<int, int>> results = [];
+      expect(results.getAllErrors(), isEmpty);
+    });
+
+    test('should return empty list if has only success', () {
+      List<Result<int, int>> results = [const Ok(1), const Ok(2)];
+      expect(results.getAllErrors(), isEmpty);
     });
   });
 }
